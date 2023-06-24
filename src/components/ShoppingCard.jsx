430bfac12393ba6dca4c53196fbb03cfc4c7f917
LONEWOLF143 <twaha2030@gmail.com>
2023-06-24 19:55:01 +0300

    added shopping card items

--- src/components/ShoppingCard.jsx
+++ src/components/ShoppingCard.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Card, CardContent, Typography, IconButton, Grid } from '@mui/material';
+import { ShoppingCart, Favorite } from '@mui/icons-material';
 
-const ShoppingCard = ({product}) => {
+const ShoppingCard = ({ product }) => {
+  const { image, description, price, rating } = product;
+
   return (
-   <div key={product.id}>
-    <h1>{product.title}</h1>
-    </div>
+    <Card>
+      <CardContent>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item>
+            <img src={image} alt="Product" style={{ width: '100px', height: '100px' }} />
+          </Grid>
+          <Grid item>
+            <Typography variant="h6">Product Name</Typography>
+            <Typography variant="body2" color="textSecondary">
+              {description.toString()}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              Price: {price.toString()}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              Rating: {rating.toString()}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <IconButton color="primary" aria-label="Add to cart">
+              <ShoppingCart />
+            </IconButton>
+            <IconButton color="secondary" aria-label="Add to favorites">
+              <Favorite />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 };
 
